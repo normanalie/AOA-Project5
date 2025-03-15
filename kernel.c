3857dc6b8972486f4d6a1d73d420a8a7304ac1d9
Mathieu Akoun <mathieu.akoun@ens.uvsq.fr>
2025-03-15 11:18:22 +0100

    OPT1 of the baseline kernel function

--- kernel.c
+++ kernel.c
@@ -1,7 +1,23 @@
 
 #ifdef OPT1
 
-// TODO
+#include <math.h> // sqrt
+
+float kernel (unsigned n, const double a[n][n], const float b[n]) {
+    unsigned i, j;
+    float c[n];
+    float s = 0.0;
+
+    for(j=0; j<n; j++){
+        c[j] = sqrt(b[j]);
+    }
+
+    for (j=0; j<n; j++)
+        for (i=0; i<n; i++)
+            s += a[i][j] / c[i];
+
+    return s;
+}
 
 #elif defined OPT2
 
