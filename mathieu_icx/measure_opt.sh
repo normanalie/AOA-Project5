be89bfe25b7ba4ae189aea5c1f2269bdfde48a9a
Mathieu Akoun <mathieu.akoun@ens.uvsq.fr>
2025-03-24 23:55:14 +0100

    Cleaning

--- mathieu_icx/measure_opt.sh
+++ mathieu_icx/measure_opt.sh
@@ -4,15 +4,16 @@
 # $2 = valeur de n telle que T(n) ~= 90% de L1
 # $3 = valeur de n telle que T(n) ~= 50% de L3
 
-val=10
+val=100
 sudo cpupower -c all frequency-set -g performance
 
+make clean && make 
 mkdir -p measured_values_opt
-mkdir -p measured_values_opt/$1
+mkdir -p measured_values_opt/measures_$1
 
     # Exécute la commande avec taskset sur le cœur 1 et redirige la sortie vers un fichier
-    taskset -c 1 bin/measure $2 100 $val > "/home/mathieu/Downloads/AOA-Project5/mathieu/measured_values_opt/${1}/measure_$2_100_${val}.txt" 2>&1
+    taskset -c 1 bin/measure $2 100 $val > "/home/mathieu/Downloads/AOA-Project5/mathieu_icx/measured_values_opt/measures_${1}/measure_$2_100_${val}.txt" 2>&1
     echo "Execution en ${1} avec n=${2}"
-    taskset -c 1 bin/measure $3 100 $val > "/home/mathieu/Downloads/AOA-Project5/mathieu/measured_values_opt/${1}/measure_$3_100_${val}.txt" 2>&1
+    taskset -c 1 bin/measure $3 100 $val > "/home/mathieu/Downloads/AOA-Project5/mathieu_icx/measured_values_opt/measures_${1}/measure_$3_100_${val}.txt" 2>&1
     echo "Execution en ${1} avec n=${3}"
     sleep 0.1
