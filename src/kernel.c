97838403fd97abd14eaa5fcc30bffcfd37b0b0b3
mathieu-akoun <mathieu.akoun@ens.uvsq.fr>
2025-03-31 15:40:31 +0200

    Cleaning of OPT1 (must split OPT2 optimisations

--- src/kernel.c
+++ src/kernel.c
@@ -1,57 +1,41 @@
 
-#ifdef OPT1
+#ifdef OPT1 //inverser les indices des boucles pour s'accorder à la lecture ligne par ligne (row-major order)
 
 #include <math.h> // sqrt
 
-float kernel(unsigned n, const double a[n][n], const float b[n]) {
+float kernel (unsigned n, const double a[n][n], const float b[n]) {
     unsigned i, j;
     float s = 0.0;
 
-    // Pré-calcul des sqrt(b[i]) pour éviter les recalculs
-    float sqrt_b[n];
-    for (i = 0; i < n; i++) {
-        sqrt_b[i] = sqrt(b[i]); // Risque de NaN si b[i] < 0
-    }
-
-    // Calcul de la somme
-    for (j = 0; j < n; j++) {
-        for (i = 0; i < n; i++) {
-            s += a[i][j] / sqrt_b[i]; // Utilisation du pré-calcul
-        }
-    }
+    for (i=0; i<n; i++)
+        for (j=0; j<n; j++)
+            s += a[i][j] / sqrt(b[i]);
 
     return s;
 }
 
-#elif defined OPT2
+#elif defined OPT2 //pré-calcul de l'inverse de la racine carrée des valeurs de b afin de réduire le nombre de calculs
 
-// TODO
 #include <math.h> // sqrt
 
-float kernel(unsigned n, const double a[restrict n][n], const float b[restrict n]) {
+
+
+float kernel(unsigned n, const double a[n][n], const float b[n]) {
     unsigned i, j;
     float s = 0.0;
 
     // Pré-calcul de 1 / sqrt(b[i]) pour éviter les divisions
     float inv_sqrt_b[n];
     for (i = 0; i < n; i++) {
-        inv_sqrt_b[i] = 1.0f / sqrtf(b[i]); // Évite division dans la boucle principale
+        inv_sqrt_b[i] = 1.0f / sqrtf(b[i]); //calcul en float
     }
 
     // Calcul de la somme avec unrolling pour réduire les accès mémoire
-    for (j = 0; j < n; j++) {
-        for (i = 0; i + 3 < n; i += 4) { // Déroulage de boucle par 4
-            s += a[i][j] * inv_sqrt_b[i] +
-                 a[i + 1][j] * inv_sqrt_b[i + 1] +
-                 a[i + 2][j] * inv_sqrt_b[i + 2] +
-                 a[i + 3][j] * inv_sqrt_b[i + 3];
-        }
-        // Traitement des éléments restants
-        for (; i < n; i++) {
-            s += a[i][j] * inv_sqrt_b[i];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            s += (float)a[i][j] * inv_sqrt_b[i]; //je crois qu'il y a deux conversions inutiles a cause du a en double => cast en float 
         }
     }
-
     return s;
 }
 
